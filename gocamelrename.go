71dc01e65da8d677813595ec9b93d8b1ab18bc91
Matthew Zimmerman <mzimmerman@gmail.com>
2014-01-03 10:31:03 -0500

    Found some cases in files inside hidden directories that caused algorithm problems, putting a "cap" on the infinite loop and ignoring the hidden directories

--- gocamelrename.go
+++ gocamelrename.go
@@ -35,6 +35,10 @@ func main() {
 func renameFiles(fileList []os.FileInfo) error {
 	for _, fi := range fileList {
 		if fi.IsDir() {
+			if string(fi.Name()[0]) == "." {
+				// skip this, it's a hidden directory
+				continue
+			}
 			if recursive {
 				files, err := ioutil.ReadDir(fi.Name())
 				if err != nil {
@@ -45,7 +49,10 @@ func renameFiles(fileList []os.FileInfo) error {
 						log.Printf("Could not change working directory to %s, skipping it - %v", fi.Name(), err)
 						return nil
 					} else {
-						return renameFiles(files)
+						err := renameFiles(files)
+						if err != nil {
+							log.Fatalf("Could not change to a parent directory we've already been to, failing - %v", err)
+						}
 					}
 				}
 			}
@@ -54,6 +61,10 @@ func renameFiles(fileList []os.FileInfo) error {
 		orig := fi.Name()
 		camelName := strings.Replace(orig, string(orig[0]), strings.ToUpper(string(orig[0])), 1)
 		for x := 1; x < len(camelName); x++ { // start at one since first char is uppercased already
+			if len(camelName) > len(orig)*2 {
+				log.Printf("Working on char[%d] %s in %s", x, string(camelName[x]), camelName)
+				log.Fatalf("Something went wrong, do not continue on file %s", orig)
+			}
 			if strings.ContainsAny(string(camelName[x]), "ABCDEFGHIJKLMNOPQRSTUVWXYZ") && string(camelName[x-1]) != " " {
 				camelName = strings.Replace(camelName, string(camelName[x]), " "+strings.ToUpper(string(camelName[x])), 1)
 			}
